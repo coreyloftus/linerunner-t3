5fd382fb81697fd4f846add9bbb718d7f82c20c7
coreyloftus <112141387+coreyloftus@users.noreply.github.com>
2024-11-06 18:45:08 -0500

    update config for deploy

--- next.config.js
+++ next.config.js
@@ -2,9 +2,27 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Convert import.meta.url to __dirname equivalent
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "~": path.resolve(__dirname, "./src"),
+    };
+    return config;
+  },
+};
 
 export default config;
