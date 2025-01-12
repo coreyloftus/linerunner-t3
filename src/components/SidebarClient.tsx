c171838fdf1a126ba2ea5ede2219a8cb2d795bf6
Corey Loftus <112141387+coreyloftus@users.noreply.github.com>
2025-01-12 18:50:50 -0500

    remove padding from sidebarClient, sits at top of page now

--- src/components/SidebarClient.tsx
+++ src/components/SidebarClient.tsx
@@ -11,23 +11,26 @@ export function SidebarClient({ projects, allData }: SidebarClientProps) {
   const [navOpen, setNavOpen] = useState(false);
   return (
     <>
-      <div className="fixed left-0 top-0 z-20 p-4">
+      <div className="fixed left-0 top-0 z-20">
         <Button
           onClick={() => setNavOpen(!navOpen)}
           className={`bg-stone-500 text-white`}
         >
-          <h1 className="text-2xl">{!navOpen ? "→" : "←"}</h1>
+          <h1 className="text-2xl">{!navOpen ? "→" : "X"}</h1>
         </Button>
       </div>
       {/* sidebar */}
       <div
-        className={`fixed left-0 top-4 h-full w-[33vw] transform rounded-md border-r-2 border-t-2 border-stone-200 bg-stone-400 transition-all duration-500 ease-in-out ${
+        className={`fixed left-0 top-0 h-full w-[33vw] transform rounded-md border-r-2 border-t-2 border-stone-200 bg-stone-400 transition-all duration-500 ease-in-out ${
           navOpen ? "translate-x-0 opacity-100" : "-translate-x-full opacity-0"
         }`}
         style={{ zIndex: 15 }}
       >
-        <div className="pt-12">
+        <div className="pt-10">
           <NewScriptSelect projects={projects} allData={allData} />
+        </div>
+        <div className="fixed bottom-0 pb-2 pl-2 font-mono text-sm">
+          LineRunner by Corey -- ©2025
         </div>
       </div>
       {/* overlay */}
