7e073aef90c8435858df95143984996309cebb9b
coreyloftus <coreyloftus@gmail.com>
2025-08-13 10:58:30 -0400

    add custom favicon

---
+++ src/styles/faviconIco.tsx
@@ -0,0 +1,18 @@
+<svg
+  width="32"
+  height="32"
+  viewBox="0 0 32 32"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  {/* Black background */}
+  <rect width="32" height="32" fill="#000000" />
+
+  {/* Yellow highlighted line (current line) */}
+  <rect x="4" y="8" width="20" height="3" rx="1.5" fill="#FFD700" />
+
+  {/* White line 1 */}
+  <rect x="12" y="15" width="16" height="3" rx="1.5" fill="#FFFFFF" />
+
+  {/* White line 2 */}
+  <rect x="10" y="22" width="18" height="3" rx="1.5" fill="#FFFFFF" />
+</svg>;
